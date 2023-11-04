7f255ce1b5574fb8b0a55a46bc09140baebb647f
Biterider <Biterider@gmx.net>
2023-11-04 12:37:26 +0100

    Compressed cursor added

---
+++ Code/ObjMem/32/CreateCursorFromCompressedFileInRes.asm
@@ -0,0 +1,29 @@
+; ==================================================================================================
+; Title:      CreateCursorFromCompressedFileInRes.asm
+; Author:     G. Friedrich
+; Version:    C.1.0
+; Notes:      Version C.1.0, October 2023
+;               - First release.
+; Links:      https://learn.microsoft.com/en-us/windows/win32/api/compressapi/
+; ==================================================================================================
+
+
+% include @Environ(OBJASM_PATH)\\Code\\OA_Setup32.inc
+TARGET_STR_TYPE = STR_TYPE_WIDE
+% include &ObjMemPath&ObjMemWin.cop
+
+.code
+; ——————————————————————————————————————————————————————————————————————————————————————————————————
+; Procedure:  CreateCursorFromCompressedFileInRes
+; Purpose:    Create a cursor from a compressed cursor file stored as RCDATA resource. 
+;             Compression should be done using a Microsoft compression algorithm: XPRESS, XPRESS
+;             with Huffman encoding, MSZIP or LZMS.
+; Arguments:  Arg1: hInstance.
+;             Arg2: Resource ID.
+;             Arg3: Desired cursor width.
+;             Arg3: Desired cursor height.
+; Return:     xax = Cursor HANDLE.
+
+% include &ObjMemPath&Common\CreateCursorFromCompressedFileInRes_X.inc
+
+end
